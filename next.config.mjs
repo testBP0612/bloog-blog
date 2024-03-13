fbf36b1657156e83c802053569f7eefdadafea5f
Bloop <bloophuang@wondercise.com>
2024-03-13 13:49:18 +0800

    feat: install and config svgr

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,27 @@ import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import createMDX from '@next/mdx';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// MDX configuration
+const mdxConfig = {
+  extension: /\.mdx?$/,
+  remarkPlugins: [remarkGfm],
+  rehypePlugins: [rehypeHighlight],
+  providerImportSource: '@mdx-js/react',
+};
 
-const withMDX = createMDX({
-  options: {
-    extension: /\.mdx?$/,
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeHighlight],
-    providerImportSource: '@mdx-js/react',
-  },
-});
+const withMDX = createMDX({ options: mdxConfig });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config;
+  }
+};
+
 export default withMDX(nextConfig);
