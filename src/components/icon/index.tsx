36c336109602e569be3424984c1b9ea729867eba
Bloop <bloophuang@wondercise.com>
2024-03-15 18:04:35 +0800

    feat: add icon to Footer

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,7 @@
+import LinkedInIcon from '@components/icon/images/linkedIn.svg';
+import GitHubIcon from '@components/icon/images/github.svg';
+
+export {
+	LinkedInIcon,
+	GitHubIcon,
+}
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,35 @@
 import Link from 'next/link';
 
+import Heading from '@components/UI/Heading';
+import { GitHubIcon, LinkedInIcon } from '@components/icon';
 import { containerClassName } from '@components/layout/MainContainer';
 
 export default function Footer() {
   return (
     <footer className={containerClassName}>
-      <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4"></div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-300">
-          <div>author Bloop</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">title</Link>
+      <div className="grid grid-cols-2 gap-8 py-16 md:grid-cols-4 md:gap-0">
+        <div className="col-span-2 flex flex-col text-sm text-gray-500 dark:text-gray-300 items-center md:items-start">
+          <Heading level="h5">Bojun Huang</Heading>
+          <span>{`© ${new Date().getFullYear()}`}</span>
+          <span>
+            powered by <Link href="https://nextjs.org/">Next.js</Link>. All rights reserved.
+          </span>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-300">
-          <Link href="">Tailwind Nextjs Theme</Link>
+        <div className="flex flex-col text-sm gap-1 text-gray-500 dark:text-gray-300 items-center md:items-end">
+          <span className="text-gray-500 dark:text-gray-300 opacity-60 mb-1">Pages</span>
+          <Link href="/about">About</Link>
+          <Link href="/blog">Blog</Link>
+        </div>
+        <div className="flex flex-col text-sm gap-1 text-gray-500 dark:text-gray-300 items-center md:items-end">
+          <span className="text-gray-500 dark:text-gray-300 opacity-60 mb-1">Social</span>
+          <Link className="flex items-center space-x-1" href="/">
+            <LinkedInIcon className="w-4 h-4 fill-slate-500 dark:fill-slate-200" />
+            <span>LinkedIn</span>
+          </Link>
+          <Link className="flex items-center space-x-1" href="/">
+            <GitHubIcon className="w-4 h-4 fill-slate-500 dark:fill-slate-200" />
+            <span>GitHub</span>
+          </Link>
         </div>
       </div>
     </footer>
