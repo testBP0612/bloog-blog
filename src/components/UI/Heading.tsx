95d74f9d0e119e0c6c962652872e0cdd75aeb5d2
Bloop <bloophuang@wondercise.com>
2024-04-12 11:35:23 +0800

    feat: update Footer and Heading styles

--- src/components/UI/Heading.tsx
+++ src/components/UI/Heading.tsx
@@ -10,13 +10,13 @@ const levelStyles = {
   h1: 'text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 mb-6 md:mb-8',
   h2: 'text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-9 md:text-5xl md:leading-12 mb-2 md:mb-4',
   h3: 'text-xl font-bold leading-7 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-8 md:text-4xl md:leading-10 mb-2 md:mb-4',
-  h4: 'text-lg font-bold leading-6 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-7 md:text-3xl md:leading-9 mb-2 md:mb-4',
+  h4: 'text-lg font-semibold leading-6 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-7 md:text-3xl md:leading-9 mb-2 md:mb-4',
   h5: 'text-base font-semibold leading-2 tracking-tight text-gray-900 dark:text-gray-100 md:leading-4 mb-1 md:mb-2',
 };
 
-const Heading = ({ level, className, children }: HeadingProps) => {
+const Heading = ({ level, className = '', children }: HeadingProps) => {
   const Tag = level as keyof JSX.IntrinsicElements;
-  const defaultClassName = levelStyles[level] + (className ? ` ${className}` : '');
+  const defaultClassName = `${levelStyles[level]} ${className}`;
 
   return (
     <Tag className={defaultClassName}>{children}</Tag>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,29 +7,34 @@ import { containerClassName } from '@components/layout/MainContainer';
 export default function Footer() {
   return (
     <footer className={containerClassName}>
-      <div className="grid grid-cols-2 gap-8 py-16 md:grid-cols-4 md:gap-0">
-        <div className="col-span-2 flex flex-col text-sm text-gray-500 dark:text-gray-300 items-center md:items-start">
+      <div className="grid grid-cols-2 gap-y-4 py-10 md:py-16 md:grid-cols-4 md:gap-0">
+        <div className="col-span-2 text-sm text-gray-500 dark:text-gray-300 m-auto md:m-0">
           <Heading level="h5">Bojun Huang</Heading>
           <span>{`© ${new Date().getFullYear()}`}</span>
+          &nbsp;
           <span>
             powered by <Link href="https://nextjs.org/">Next.js</Link>. All rights reserved.
           </span>
         </div>
-        <div className="flex flex-col text-sm gap-1 text-gray-500 dark:text-gray-300 items-center md:items-end">
-          <span className="text-gray-500 dark:text-gray-300 opacity-60 mb-1">Pages</span>
-          <Link href="/about">About</Link>
-          <Link href="/blog">Blog</Link>
+        <div className="flex flex-col text-sm text-gray-500 dark:text-gray-300 items-center md:items-end">
+          <div>
+            <span className="block text-gray-500 dark:text-gray-300 opacity-60 mb-1.5">Pages</span>
+            <Link className="block mb-1 hover:underline" href="/about">About</Link>
+            <Link className="block mb-1 hover:underline" href="/blog">Blog</Link>
+          </div>
         </div>
-        <div className="flex flex-col text-sm gap-1 text-gray-500 dark:text-gray-300 items-center md:items-end">
-          <span className="text-gray-500 dark:text-gray-300 opacity-60 mb-1">Social</span>
-          <Link className="flex items-center space-x-1" target="_blank" href="https://www.linkedin.com/in/%E6%9F%8F%E9%88%9E-%E9%BB%83-731848154/">
-            <LinkedInIcon className="w-4 h-4 fill-slate-500 dark:fill-slate-200" />
-            <span>LinkedIn</span>
-          </Link>
-          <Link className="flex items-center space-x-1" target="_blank" href="https://github.com/testBP0612">
-            <GitHubIcon className="w-4 h-4 fill-slate-500 dark:fill-slate-200" />
-            <span>GitHub</span>
-          </Link>
+        <div className="flex flex-col text-sm text-gray-500 dark:text-gray-300 items-center md:items-end">
+          <div>
+            <span className="block text-gray-500 dark:text-gray-300 opacity-60 mb-1.5">Social</span>
+            <Link className="relative flex items-center mb-1 hover:underline" target="_blank" href="https://www.linkedin.com/in/%E6%9F%8F%E9%88%9E-%E9%BB%83-731848154/">
+              <LinkedInIcon className="absolute -left-5 w-4 h-4 fill-slate-500 dark:fill-slate-200" />
+              <span>LinkedIn</span>
+            </Link>
+            <Link className="relative flex items-center mb-1 hover:underline" target="_blank" href="https://github.com/testBP0612">
+              <GitHubIcon className="absolute -left-5 w-4 h-4 fill-slate-500 dark:fill-slate-200" />
+              <span>GitHub</span>
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
