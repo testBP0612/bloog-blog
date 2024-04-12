389ec3dd85487e5411a268877f1e647ccb12a513
Bloop <bloophuang@wondercise.com>
2024-04-12 13:58:30 +0800

    fix: sitemap url error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,22 @@
-import { MetadataRoute } from "next";
+import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	return [
-		{
-			url: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
-			lastModified: new Date(),
-			priority: 1,
-		},
-		{
-			url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/about`,
-			lastModified: new Date(),
-			priority: 0.8,
-		},
-		{
-			url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/blog`,
-			lastModified: new Date(),
-			changeFrequency: 'monthly',
-			priority: 0.6,
-		}
-	]
+  return [
+    {
+      url: "https://www.testbp.xyz",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://www.testbp.xyz/about",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.testbp.xyz/blog",
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+  ];
 }
